1fbdf113311d9880a6a7d31c969ff68467e810ff
Carolina Rossello <c.rossello@ufl.edu>
2023-02-05 13:18:18 -0500

    pages update

---
+++ src/pages/about.jsx
@@ -0,0 +1,8 @@
+
+export default function About() {
+  return (
+    <main>
+      <h1>I'm the About page!</h1>
+    </main>
+  )
+}
---
+++ src/pages/contact.jsx
@@ -0,0 +1,8 @@
+
+export default function Contact() {
+  return (
+    <main>
+      <h1>I'm the Contact page!</h1>
+    </main>
+  )
+}
