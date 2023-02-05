e209304e9316a2c8b905571fde264c0b2688ce1b
Carolina Rossello <c.rossello@ufl.edu>
2023-02-05 13:34:40 -0500

    added Link - import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
