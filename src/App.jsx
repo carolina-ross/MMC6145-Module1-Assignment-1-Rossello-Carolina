30953b32d0164d7d6e6145ada16276138083ba99
Carolina Rossello <c.rossello@ufl.edu>
2023-02-05 13:31:14 -0500

    app set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
 
 export default function App() {
   return (
     <>
+        <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <a href="/contact">Contact</a>
+      </header>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   )
