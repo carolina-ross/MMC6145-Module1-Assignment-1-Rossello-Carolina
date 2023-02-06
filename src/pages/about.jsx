f7994cd40d933b1f0181ec12bf101907797dddf7
Carolina Rossello <c.rossello@ufl.edu>
2023-02-06 03:21:55 -0500

    about

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,7 +3,10 @@ import styles from '../styles/about.css';
 export default function About() {
   return (
     <main>
-      <h1>I'm the About page!</h1>
-    </main>
+      <div className={styles.about-container}>
+      <h1 className={styles.title}>Carolina Rossello</h1>
+      <p className={styles.about-text}>I am currently pursuing my Master's degree at the University of Florida. I am passionate about technology and continuously strive to expand my knowledge and skills. Along with my studies, I am working at a marketing company where I have the opportunity to apply my skills and knowledge in a professional setting. My goal is to continue growing in my career while also mastering my coding skills. With a passion for technology and design, I aim to make a positive impact in the marketing industry.</p>
+      </div>
+      </main>
   )
 }
