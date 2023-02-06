8f7a4557bf169bdeeaa851c9709dc57526a59571
Carolina Rossello <c.rossello@ufl.edu>
2023-02-06 00:53:09 -0500

    App & Home

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes, Link } from 'react-router-dom'
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
+import Header from "./components/header"
+
+
 
 export default function App() {
   return (
@@ -17,6 +20,7 @@ export default function App() {
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+
     </>
   )
 }
