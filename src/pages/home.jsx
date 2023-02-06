534651422b6e432b17b314a005fc2137a4a62362
Carolina Rossello <c.rossello@ufl.edu>
2023-02-06 03:05:19 -0500

    home with project links pre-set

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,28 @@
-import styles from '../styles/home.module.css'
+import styles from '../styles/home.module.css';
+import React from 'react';
+import ProjectPreview from '../components/projects'; 
 
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.placeholder}>I'm the home page!</h1>
+      <h1 className={styles.title}>Welcome to my Portfolio Site</h1>
+      <p>Here are some of my projects:</p>
+<ul>
+<li>
+<ProjectPreview 
+         projectName="Project 1"
+         projectDescription="This is a description of Project 1"
+         projectLink="#"
+       />
+</li>
+<li>
+<ProjectPreview 
+         projectName="Project 2"
+         projectDescription="This is a description of Project 2"
+         projectLink="#"
+       />
+</li>
+</ul>
     </main>
   )
 }
