49b4bcf97f4408093b4bf1c9b5a8a421caa879c5
Carolina Rossello <c.rossello@ufl.edu>
2023-02-09 03:27:43 -0500

    added projects

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
 import Header from "./components/header"
-
+import Projects from "./components/projects"
 
 export default function App() {
   return (
@@ -14,6 +14,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
 
     </>
