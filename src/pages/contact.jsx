129a97aa499df74892d7c8a0acbee6fc8c2f0593
Carolina Rossello <c.rossello@ufl.edu>
2023-02-09 01:28:38 -0500

    contact page

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,8 +1,21 @@
+import styles from '../styles/contact.module.css';
+import React from 'react';
 
 export default function Contact() {
   return (
-    <main>
-      <h1>I'm the Contact page!</h1>
+    <main className={styles.container}>
+      <h1 className={styles.title}>Here you can find the links to my LinkedIn and GitHub</h1>
+      <div className={styles.iconContainer}>
+      <a href="https://www.linkedin.com/in/carolina-rossello-46b520241/" target="_blank">
+           <img className={styles.icon} src='./src/components/linkedin.png'  alt="LinkedIn profile" />
+        </a>
+
+        <a href="https://github.com/carolina-ross" target="_blank">
+        <img className={styles.icon} src="./src/components/github.png" alt="GitHub profile" />
+        </a>
+
+      </div>
     </main>
   )
 }
+
