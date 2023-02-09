5d182ccc18c5d34bfbc1307ee3850bd62360133d
Carolina Rossello <c.rossello@ufl.edu>
2023-02-09 03:27:22 -0500

    project file

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styles from '../styles/projects.css';
 
 export default function ProjectPreview({ projectName, projectDescription, projectLink }) {
     return (
       <>
+      
+      <div className="projectPreview">
         <h3>{projectName}</h3>
         <p>{projectDescription}</p>
         <a href={projectLink}>View project</a>
+        </div>
+        
       </>
     )
   }
